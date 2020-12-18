beffc5544e6c5654577238d717fb7976638518c6
kmt_kin <63468364+KinMingTsang@users.noreply.github.com>
2020-12-18 22:36:21 +0800

    Update user.c

--- starter-code/user.c
+++ starter-code/user.c
@@ -10,17 +10,145 @@
 #include "user.h"
 #include "emulator.h"
 #include "qdbmp.h"
-char obsticlepositions[100]; //(x,y,type)
-int obsticle_counter;
-char ballposition[100]; //in format (x, y , type)
-char currentposition[1] = {"(0, 0)"};
-bool tennis;
-int ballcounter;
-bool tabletennis;
+float obsticlepositions[100][3]; //(x,y,0=point 1=bar)
+int obsticle_counter = 0;
+float ballposition[100][3];           //in format (x, y,0 = tennis 1 = table tennis)
+float currentposition[1][2] = {[1, 1]}; //in format (x,y)
+int ballcounter = 0;
 int threshold_for_colour_detection = 10;
 
 extern uint64_t iter; //  This variable keeps track of how many times loop() was run.
                       //  Feel free to use it.
+
+/***************************************************************************
+ ToDO: 
+    1. Smooth path planning
+    2. Rotation angle calculation
+    3. Continuously Update the current position of the car
+ ***************************************************************************/
+void moveto(int x, int y) //path planning
+{
+}
+
+/***************************************************************************
+ ToDO: 
+    1. Setting the Speed of the motor 
+ ***************************************************************************/
+void Speedcontrol(int Lspeed,int Rspeed)
+{
+
+}
+
+/***************************************************************************
+ ToDO: 
+    1. Need to figure our  x y coordinate
+    2. Need to evaluate the distance with the motor speed
+ ***************************************************************************/
+void writeposition(int store[], int distance, int message, int currentcounter)
+{
+    if (message == 3) //Three means this is an update for current position
+    {
+        store[0][0] = currentposition[0][0] + distance;
+        store[0][1] = currentposition[0][1] + distance;
+    }
+    else
+    {
+        store[currentcounter][0] = currentposition[0][0]+distance;
+        store[currentcounter][1] = currentposition[0][1]+distance;
+        store[currentcounter][2] = message ;
+        currentcounter++;
+    }
+}
+
+/***************************************************************************
+ ToDO: 
+    1. Need to modify the IR sensor detection
+    2. Estimate the distance more correct
+ ***************************************************************************/
+
+void obsticle_detection(int x, int y)
+{
+    for (int i = 0; i < sizeof(obsticlepositions); i++) //Check the current storage
+    {
+        if ((x == obsticlepositions[i][0] && (y == obsticleposition[0][1])))
+        {
+            moveto(x - 1, y);
+            break;
+        }
+    }
+    if (em_write(4, 15) ) // IR sensor detection
+    {
+        writeposition(obsticlepositions, 0.3, 0, obsticle_counter);
+        obsticle_counter++;
+        moveto(x, y);
+    }
+    else if(em_write(4, 16))
+    {
+        
+    }
+}
+/***************************************************************************
+ ToDO: 
+   Grabbing feature for the balls
+ ***************************************************************************/
+void grabbing(bool grabcommand, int type)
+{
+    if (grabcommand)
+    {
+        emwrite(10, type);
+        if (!grab_code(type)) //Regrabbing if the position is not correct.
+        {
+            moveto(currentposition[1] + 0.2);
+            grabbing(grabcommand, type);
+        }
+    }
+}
+
+
+/************************************************************************************
+ 1. Determine whether it is a tennis ball or table tennis ball
+ 2. Store the position of the ball
+ ************************************************************************************/
+
+void indentify_size_of_ball(BMP *bmp)
+{
+    int red, green, blue;
+    float avgred, avggreen, avgblue;
+
+    for (int i = 0; i < BMP_GetHeight(bmp); i++)
+    {
+        for (int j = 0; j < BMP_GetWidth(bmp); j++)
+        {
+            for (a = 0; a < 3; a++)
+            {
+                for (b = 0; b <= 3; b++)
+                {
+                    BMP_GetPixelRGB(bmp, i + a, j + b, red, green, blue);
+                    avggreen += green;
+                    avgred += red;
+                    avgblue += blue;
+                }
+            }
+            avgred = avgred / 9;
+            avggreen = avggreen / 9;
+            avgblue = avgblue / 9
+
+            if (((avgred - 255) * (avgred - 255) + (avggreen - 201) * (avggreen - 201) + (avgblue - 92) * (avgblue - 92)) < 50) //detecting the colour difference between table tennis and current colour
+            {
+                writeposition(ballposition,1,1,ballcounter);
+            }
+            else if (((avgred - 190) * (avgred - 190) + (avggreen - 193) * (avggreen - 193) + (avgblue - 54) * (avgblue - 54)) < 50)
+            {
+                writeposition(ballposition,1,0,ballcounter);
+                break;
+            }
+            avgblue  = 0;
+            avggreen = 0;
+            avgred   = 0;
+        }
+    }
+}
+
 
 void setup(void)
 {
@@ -38,110 +166,6 @@ void setup(void)
     emwrite(2, 1, 0.5);
     //  N.B. You can only initialise and configure components within this function.
     //       (Of course, you can still call another function which does the init.)
-}
-
-/************************************************************************************
- 1. Determine whether it is a tennis ball or table tennis ball
- 2. Store the position of the ball
- ************************************************************************************/
-
-void indentify_size_of_ball(BMP *bmp)
-{
-    int red, green, blue, avgred, avggreen, avgblue;
-
-    for (int i = 0; i < BMP_GetHeight(camera.jpg); i++)
-    {
-        for (int j = 0; j < BMP_GetWidth(camera.jpg); j++)
-        {
-            for (a = 0; a < 3; a++)
-            {
-                for (b = 0; b <= 3; b++)
-                {
-                    BMP_GetPixelRGB(bmp, i + a, j + b, red, green, blue);
-                    avggreen += green;
-                    avgred += red;
-                    avgblue += blue;
-                }
-            }
-
-            ball_type_detection(avgred / 9, avggreen / 9, avgblue / 9);
-            if (tennis)
-            {
-                ballposition[ballcounter] = currenposition[0] + 1;
-                ballcounter++;
-            }
-            else if (tabletennis)
-            {
-                ballposition[ballcounter] == currentposition[0] + 1;
-            }
-        }
-    }
-}
-
-void ball_type_detection(float red, float green, float blue)
-{
-    float error_between_tabletennis = (red - 190) * (red - 190) + (green - 193) * (green - 193) + (blue - 54) * (blue - 54);
-    float error_between_tennis = (red - 255) * (red - 255) + (green - 201) * (green - 201) + (blue - 92) * (blue - 92);
-    float error_between_tabletennis2 = (red - 247) * (red - 247) + (green - 245) * (green - 245) + (blue - 250) * (blue - 250);
-    //Sample error : ~~59
-    if ((error_between_tabletennis <= threshold_for_colour_detection * threshold_for_colour_detection) && (error_between_tabletennis > 65))
-    {
-        tabletennis = true;
-        tennis = false;
-    }
-    else if ((error_between_tennis <= threshold_for_colour_detection * threshold_for_colour_detection) && (error_between_tennis > 65))
-    {
-        tennis = true;
-        tabletennis = false;
-    }
-    else if ((error_between_tabletennis2 <= threshold_for_colour_detection * threshold_for_colour_detection) && (error_between_tabletennis2 > 65))
-    {
-        tabletennis = true;
-        tennis = false;
-    }
-}
-
-void moveto(int x, int y) //path planning
-{
-}
-
-void readposition(int stored[],int index)
-{
-}
-
-void writeposition(int store[], int distance, char message, int currentcounter)
-{
-    store[i] = (currentposition[0] + distance, message);
-}
-
-void obsticle_detection(int x,int y)
-{
-    for (int i = 0; i < sizeof(obsticlepositions); i++)//Check the current storage
-    {
-        if ((i == readpositions(,)) && (read == readposition(,)))
-        {
-            moveto(x,y);
-        }
-    }
-    if (em_write(4, 15) || em_write(4, 16)) // IR sensor detection
-    {
-        writeposition(obsticlepositions, obsticle_counter);
-        obsticle_counter++;
-        moveto(x,y);
-    }
-}
-
-void grabbing(bool grabcommand, int type)
-{
-    if (grabcommand)
-    {
-        emwrite(10, type);
-        if (!grab_code(type))
-        {
-            moveto(currentposition[1] + 0.2);
-            grabbing(grabcommand, type);
-        }
-    }
 }
 
 void loop(void)
